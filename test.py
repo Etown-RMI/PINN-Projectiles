3a1207d1f7e9fca664bf475cce5c7be7ef6a1a18
klinefelters <stevenjk03@gmail.com>
2024-01-27 01:32:49 -0500

    Working projectile motion PINN

--- test.py
+++ test.py
@@ -2,30 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pinns
 
-x = pinns.Domain(-1, 1, 100)
+x = pinns.Domain(0, 10, 100)
+accel = -9.8
+v0 = 10
+x0 = 0
 
 
 def pde(x, y):
-    dy_xx = pinns.Grad.hessian(y, x)
-    dy_xxx = pinns.Grad.jacobian(dy_xx, x)
-    return dy_xxx - 2
+    return pinns.Grad.hessian(y, x) - accel
 
 
-def ic_out(x_in, y):
-    dyx = pinns.Grad.jacobian(y, x_in)
-    return 2*dyx-5
+ic1 = pinns.IC(x_ic=0, f=v0, y_der=1)
+ic2 = pinns.IC(x_ic=0, f=x0, y_der=0)
+
+model = pinns.net(inputs=1, layers=2 * [32], activation='softplus', outputs=1)
+pinns.train(model, x, pde, [ic1, ic2], epochs=2000, lr=0.01)
 
 
-ic1 = pinns.IC(x_ic=1, f=ic_out, y_der=2)
-ic2 = pinns.IC(x_ic=-1, f=1, y_der=1)
-ic3 = pinns.IC(x_ic=-1, f=0, y_der=0)
-
-model = pinns.net(inputs=1, layers=3 * [60], activation='tanh', outputs=1)
-pinns.train(model, x, pde, [ic1, ic2, ic3], epochs=2000, lr=0.001)
-
-
-x_test = np.linspace(-1, 1, 100)
-y_true = 1/3*x_test**3 + 5/6*x_test**2 + 5/3*x_test + 7/6
+x_test = np.linspace(0, 10, 100)
+y_true = (accel/2)*x_test**2 + v0*x_test + x0
 y_pred = model(x_test)
 
 plt.plot(y_true)
